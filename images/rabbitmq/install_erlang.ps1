be73ae52476c0b23379f4a6953b783e6e49c2981
Feodor Fitsner <feodor@appveyor.com>
2016-10-29 15:24:59 -0700

    rabbitmq

---
+++ images/rabbitmq/install_erlang.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host "Installing Erlang..." -ForegroundColor Cyan
+
+Write-Host "Downloading..."
+$exePath = "$($env:USERPROFILE)\otp_win64_19.1.exe"
+(New-Object Net.WebClient).DownloadFile('http://erlang.org/download/otp_win64_19.1.exe', $exePath)
+
+Write-Host "Installing..."
+cmd /c start /wait $exePath /S
+del $exePath
+
+$env:path = "$env:path;C:\Program Files\erl8.1\bin"
+
+[Environment]::SetEnvironmentVariable('ERLANG_HOME', 'C:\Program Files\erl8.1', 'Machine')
+$env:ERLANG_HOME="C:\Program Files\erl8.1"
+
+# C:\Program Files\erl8.1
+
+$x64items = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+$x64items + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\CurrentVersion\Uninstall") `
+    | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
+    | Where-Object { $_.DisplayName -and $_.DisplayName.contains('Erlang') } `
+    | Sort-Object -Property DisplayName `
+    | Select-Object -Property DisplayName,DisplayVersion
+
+Write-Host "Installed Erlang" -ForegroundColor Green
---
+++ images/rabbitmq/install_rabbitmq.ps1
@@ -0,0 +1,23 @@
+﻿Write-Host "Installing RabbitMQ..." -ForegroundColor Cyan
+
+Write-Host "Downloading..."
+$exePath = "$($env:USERPROFILE)\rabbitmq-server-3.6.5.exe"
+(New-Object Net.WebClient).DownloadFile('http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.5/rabbitmq-server-3.6.5.exe', $exePath)
+
+Write-Host "Installing..."
+cmd /c start /wait $exePath /S
+
+Write-Host "RabbitMQ installed and started" -ForegroundColor Green
+
+
+
+Write-Host "Installing RabbitMQ plugins..." -ForegroundColor Cyan
+
+Write-Host "Downloading..."
+$pluginPath = "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.6.5\plugins\rabbitmq_delayed_message_exchange-0.0.1.ez"
+(New-Object Net.WebClient).DownloadFile('https://bintray.com/rabbitmq/community-plugins/download_file?file_path=rabbitmq_delayed_message_exchange-0.0.1.ez', $pluginPath)
+
+Write-Host "Installing..."
+& "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.6.5\sbin\rabbitmq-plugins.bat" enable rabbitmq_delayed_message_exchange --offline
+
+Write-Host "RabbitMQ plugins installed and started" -ForegroundColor Green
