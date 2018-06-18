75d011c2310343c2536bf46652b64dc3ce5ccf2b
Feodor Fitsner <feodor@appveyor.com>
2018-06-18 07:37:32 -0700

    RabbitMQ image updated

--- images/rabbitmq/install_rabbitmq.ps1
+++ images/rabbitmq/install_rabbitmq.ps1
@@ -1,23 +1,36 @@
-﻿Write-Host "Installing RabbitMQ..." -ForegroundColor Cyan
+﻿# https://www.rabbitmq.com/install-windows-manual.html
+# https://www.rabbitmq.com/install-windows.html
+
+Write-Host "Installing RabbitMQ..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
-$exePath = "$($env:USERPROFILE)\rabbitmq-server-3.6.5.exe"
-(New-Object Net.WebClient).DownloadFile('http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.5/rabbitmq-server-3.6.5.exe', $exePath)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$zipPath = "$env:TEMP\rabbitmq-server.zip"
+Invoke-WebRequest -Uri $env:rabbit_download_url -OutFile $zipPath
 
 Write-Host "Installing..."
-cmd /c start /wait $exePath /S
+Expand-Archive -Path $zipPath -DestinationPath "c:\\"
+Remove-Item -Force $zipPath
+Rename-Item c:\rabbitmq_server-$env:RMQ_VERSION c:\rabbitmq
 
-Write-Host "RabbitMQ installed and started" -ForegroundColor Green
-
-
+Write-Host "RabbitMQ installed" -ForegroundColor Green
 
 Write-Host "Installing RabbitMQ plugins..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
-$pluginPath = "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.6.5\plugins\rabbitmq_delayed_message_exchange-0.0.1.ez"
-(New-Object Net.WebClient).DownloadFile('https://bintray.com/rabbitmq/community-plugins/download_file?file_path=rabbitmq_delayed_message_exchange-0.0.1.ez', $pluginPath)
+$zipPath = "$env:TEMP\delayed_message_exchange.zip"
+Invoke-WebRequest -Uri $env:delayed_message_exchange_url -OutFile $zipPath
 
 Write-Host "Installing..."
-& "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.6.5\sbin\rabbitmq-plugins.bat" enable rabbitmq_delayed_message_exchange --offline
+Expand-Archive -Path $zipPath -DestinationPath "c:\rabbitmq\plugins"
+Remove-Item -Force $zipPath
 
-Write-Host "RabbitMQ plugins installed and started" -ForegroundColor Green
+Write-Host "Enabling plugins..."
+& "c:\rabbitmq\sbin\rabbitmq-plugins.bat" enable rabbitmq_delayed_message_exchange --offline
+& "c:\rabbitmq\sbin\rabbitmq-plugins.bat" enable rabbitmq_management --offline
+
+Set-Content -Path 'c:\rabbitmq.config' -Value '[{rabbit, [{loopback_users, []}]}].'
+
+New-Item -ItemType Directory -Path "c:\Users\$env:USERNAME\AppData\Roaming\RabbitMQ"
+
+Write-Host "RabbitMQ plugins installed" -ForegroundColor Green
---
+++ images/rabbitmq/start.bat
@@ -0,0 +1,3 @@
+@echo off
+echo "my-cookie" > c:\Windows\.erlang.cookie
+call c:\rabbitmq\sbin\rabbitmq-server.bat
